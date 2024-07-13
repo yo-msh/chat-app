3e55d647c721b82f573c8aefba6a261bf5096ccb
manvardhan singh <manvardhansingh05@gmail.com>
2024-07-13 10:25:56 +0530

    socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -17,7 +17,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chat-app-yt.onrender.com", {
+      const socket = io("https://chat-app-prod-lctd.onrender.com", {
         query: {
           userId: authUser._id,
         },
